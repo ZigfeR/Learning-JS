51899c8d9328e9f80ba0c7f31cea884d89eab270
ZigfeR <xxxpickxxx@gmail.com>
2020-12-23 00:10:08 +0200

    Create "Printer"

---
+++ Printer/script/type.js
@@ -0,0 +1,4 @@
+var infoStartOne = document.getElementById("informer__textOne");
+var button = document.getElementById("button_value");
+
+infoStartOne.textContent = "Press the button to start Printer";
